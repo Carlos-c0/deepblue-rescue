11ea93190adecd0ab0f079f17ed3b54f9b1074c1
Carlos Crespo <cacrespo@unimagdalena.edu.co>
2026-09-14 22:30:09 -0500

    test(service): agregar esqueleto de RescueCaseServiceImplTest

--- src/test/java/com/deepblue/rescue/service/RescueCaseServiceImplTest.java
+++ src/test/java/com/deepblue/rescue/service/RescueCaseServiceImplTest.java
@@ -1,4 +1,22 @@
 package com.deepblue.rescue.service;
 
-public class RescueCaseServiceImplTest {
+import com.deepblue.rescue.mapper.RescueCaseMapper;
+import com.deepblue.rescue.repository.RescueCaseRepository;
+import com.deepblue.rescue.service.impl.RescueCaseServiceImpl;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class RescueCaseServiceImplTest {
+
+    @Mock
+    private RescueCaseRepository repository;
+
+    @Mock
+    private RescueCaseMapper mapper;
+
+    @InjectMocks
+    private RescueCaseServiceImpl service;
 }
